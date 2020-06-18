52171ea465782af185dad7e527a02c32c121ad0c
piyush1990Emilence <info@emilence.com>
2020-06-18 12:51:45 +0530

    modified hearts animation

--- Sources/FloatingHearts/FloatingHearts.swift
+++ Sources/FloatingHearts/FloatingHearts.swift
@@ -25,7 +25,7 @@ extension UIView {
         let fadeAnimation = CABasicAnimation(keyPath: "opacity")
         fadeAnimation.fromValue = 1.0
         fadeAnimation.toValue = 0.0
-        fadeAnimation.duration = 1
+        fadeAnimation.duration = 2
         fadeAnimation.beginTime = 1
         fadeAnimation.fillMode = .forwards
         fadeAnimation.isRemovedOnCompletion = false
